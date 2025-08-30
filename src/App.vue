4ba41792eb6fd6c68506ba2f19a88297675edcdd
Ivan Amon <Iab050811@gmail.com>
2025-08-30 12:57:40 +0200

    Project setup (Vue.js + Tailwind)

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <main class="min-h-screen grid place-items-center">
+    <h1 class="text-3xl font-bold">You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </main>
+</template>
+
+<style scoped></style>
