4e99e1d39bd48b7c05ee316320a5b78a143eb4a2
Ivan Amon <Iab050811@gmail.com>
2025-09-04 10:37:02 +0200

    Create Alert Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
-
 <template>
-  <nav class="mb-2 p-2 md:mb-5 md:p-5">
-    
+  <nav class="mb-1 p-2 md:p-5 l:mb-5 2xl:py-7 2xl:px-6">
+
     <div class="flex flex-row items-center gap-4">
       <a href="#" class="logo flex items-center gap-2 md:gap-3 p-2">
-        <img src="./assets/logo.svg" class="w-7 md:w-10 xl:w-12"/>
+        <img src="./assets/logo.svg" class="w-8 md:w-10 xl:w-12" />
         <span class="text-gradient font-semibold text-2xl md:text-4xl self-center">Todo List</span>
       </a>
       <a href="#" class="hidden md:block p-2 text-magic text-lg">Home</a>
@@ -14,55 +12,29 @@
     </div>
 
     <div class="flex align-middle gap-1">
-      <p class="p-2 text-user text-md md:text-lg">test@gmail.com</p>
+      <p class="p-2 text-user cursor-default text-md md:text-lg">test@gmail.com</p>
 
     </div>
 
   </nav>
 
-  <main class="px-2 pt-5 md:pt-10 md:px-8 lg:px-20 xl:px-32">
+  <main class="px-2 pt-5 md:pt-10 md:px-8 lg:px-20 xl:px-32 2xl:px-60">
 
-    <!-- <div>
-      <div>Todo title is required!</div>
-      <div>
-        <i>X</i>
-      </div>
-    </div> -->
-
-    <section class="px-6 py-3">
-
-      <form class="mt-3 mb-12 flex flex-col justify-center items-end md:flex-row md:justify-between md:items-center">
-        <div class="border-gradient w-[100%]  md:w-[70%] xl:w-[80%]">
-          <input class="todo-input" type="text" name="todo" id="todo"/>
+    <section class="px-6 py-3 mt-3">
+      <Alert :show="showAlert" message="Todo title is required!" type="warning" @close="showAlert = false"/>
+      <form class="mt-2 mb-12 flex flex-col justify-center items-end md:flex-row md:justify-between md:items-center">
+        <div class="border-gradient w-[100%]  md:w-[70%] xl:w-[75%]">
+          <input v-model="todoTitle" class="todo-input" type="text" name="todo" id="todo" />
         </div>
         <div>
-          <button class="btn mt-6 md:mt-0">Add Todo</button>
+          <button @click.prevent="addTodo" class="btn mt-6 md:mt-0 py-3 px-8 2xl:px-12">Add Todo</button>
         </div>
       </form>
-
       <ul class="gap-5 flex flex-col">
-        <li class="todo">
-          <p>Todo Title 1</p>
+        <li v-for="(todo, index) in todos" class="todo" v-bind:key="todo.id">
+          <p>{{ todo.title }}</p>
           <div>
-            <button class="btn-danger">
-              <i class="bi bi-trash"></i>
-            </button>
-          </div>
-        </li>
-
-        <li class="todo">
-          <p>Todo Title 2</p>
-          <div>
-            <button class="btn-danger">
-              <i class="bi bi-trash"></i>
-            </button>
-          </div>
-        </li>
-
-        <li class="todo">
-          <p>Todo Title 3</p>
-          <div>
-            <button class="btn-danger">
+            <button @click.prevent="removeTodo(index)" class="btn-danger">
               <i class="bi bi-trash"></i>
             </button>
           </div>
@@ -72,3 +44,44 @@
     </section>
   </main>
 </template>
+
+<script>
+
+import Alert from './components/Alert.vue';
+
+export default {
+
+  components: {
+    Alert
+  },
+
+  data() {
+    return {
+      todoTitle: '',
+      todos: [],
+      showAlert: false
+    }
+  },
+  methods: {
+
+    addTodo() {
+
+      if (this.todoTitle.trim() === '' || !this.todoTitle) {
+        this.showAlert = true;
+        return
+      }
+
+      this.todos = this.todos.concat([{
+        id: Math.floor(Math.random() * 1000),
+        title: this.todoTitle
+      }])
+
+      this.showAlert = false;
+    },
+
+    removeTodo(todoIndex) {
+      this.todos = this.todos.filter((todo, id) => id !== todoIndex)
+    }
+  }
+}
+</script>
---
+++ src/components/Alert.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="show" class="py-2">
+    <div :class="['alert', alertColor]">
+      <p>{{ message }}</p>
+      <button @click="$emit('close')" class="hover:scale-120 pt-0.5">
+        <i class="bi bi-x text-3xl"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    message: {
+      required: true,
+      type: String
+    },
+    show: {
+      required: true,
+      type: Boolean
+    },
+    type: {
+      type: String,
+      default: 'danger',
+      validator(value) {
+        return ['danger', 'warning', 'info'].includes(value)
+      }
+    }
+  },
+
+  computed: {
+    alertColor() {
+      const colors = {
+        danger: 'border-danger-500  text-danger-500 border-2',
+        warning: 'border-warning-500  text-warning-500 border-2',
+        info: 'border-gradient-500  text-gradient-500 border-2'
+      }
+      return colors[this.type] ?? colors['danger']
+    }
+  },
+
+  emits: ['close'],
+}
+</script>
+
